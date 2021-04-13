307446e1bd24e68ba3afcf09a6d8f46e497cf82f
Fabio Braga de Oliveira <fabiool@amazon.com>
2021-04-13 13:28:16 +0200

    Fix test

--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -1,13 +1,15 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, matchTemplate, MatchStyle, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as Cdk from '../lib/cdk-stack';
+import '@aws-cdk/assert/jest';
 
-test('Empty Stack', () => {
+test('Exist a thing', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new Cdk.CdkStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+
+    expect(stack).toHaveResource('AWS::IoT::Thing', {
+      ThingName: "PetTrackerThing"
+    })
 });
