f49d8c3ada2b38d859dc45079b3dfceb339f2281
Fabio Braga de Oliveira <fabiool@amazon.de>
2021-04-29 10:07:50 +0000

    Revert "Fixing issue https://github.com/aws/aws-cdk/issues/13705"
    This reverts commit 7cdbab289e8b5367ed9c8b3d79b5ad6b4ebc6c1d.

--- README.md
+++ README.md
@@ -6,6 +6,38 @@ The **PetTracker Demo** is a cloud native application built using an serverless 
 
 ![PetTracker Architecture](docs/PetTrackerArchitecture.png "PetTracker Architecture")
 
+## How to install
+
+1. In the AWS Management Console on the Services menu, navigate to [CloudShell](https://console.aws.amazon.com/cloudshell/home).
+2. Copy and past the following commands into the terminal:
+
+```shell
+curl -O https://raw.githubusercontent.com/fbdo/iot-workshop-for-pet-tracking-and-geofencing/develop/cloud9-cfn.yaml
+
+aws cloudformation create-stack --stack-name C9-ALS-Workshop --template-body file://cloud9-cfn.yaml --capabilities CAPABILITY_NAMED_IAM
+
+aws cloudformation wait stack-create-complete --stack-name C9-ALS-Workshop && echo 'Cloud9 IDE created sucessfully!'
+```
+3. When the last command returns with the success message, navigate to [Cloud9](https://console.aws.amazon.com/cloud9) and click on the "Open IDE" link on the newly created environment.
+
+4. Execute the following command in the terminal:
+
+```shell
+curl -sSL https://raw.githubusercontent.com/fbdo/iot-workshop-for-pet-tracking-and-geofencing/develop/envsetup.sh | bash -s stable
+```
+
+5. Wait for the setup, go take a coffee! And happy coding!
+
+## How to clean up
+
+1. In the AWS Management Console on the Services menu, navigate to [CloudShell](https://console.aws.amazon.com/cloudshell/home).
+2. Copy and past the following commands into the terminal:
+
+```shell
+curl -sSL https://raw.githubusercontent.com/fbdo/iot-workshop-for-pet-tracking-and-geofencing/develop/envcleanup.sh | bash -s stable
+```
+
+
 ## Technology Stack
 
 * [IoT Core](https://aws.amazon.com/iot-core/)
---
+++ cdk/lib/data-ingestion-stack.ts
@@ -0,0 +1,129 @@
+import * as cdk from '@aws-cdk/core';
+import * as iot from '@aws-cdk/aws-iot';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as iam from '@aws-cdk/aws-iam';
+
+import { CustomCertificateResource } from "./custom-certificate-resource";
+import path = require("path");
+
+export class PetTrackerDataIngestionStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    /**
+     * Create Device.
+     * The PetTrackerThing will receives messages from the tracker device or emulator.
+     */
+    const trackerThing = new iot.CfnThing(this, "IoTDevice", {
+      thingName: "PetTrackerThing"
+    });
+
+    const trackerCredentials = new CustomCertificateResource(
+      this,
+      "PetTrackerCredentials",
+      {
+        account: this.account,
+        stackName: this.stackName,
+        thingName: trackerThing.ref
+      }
+    );
+
+    new iot.CfnThingPrincipalAttachment(
+      this,
+      "PetTrackerThingCredentialAttachment",
+      {
+        principal: trackerCredentials.certificateArn,
+        thingName: trackerThing.ref
+      }
+    );
+
+    const trackerPolicy = new iot.CfnPolicy(this, "PetTrackerPolicy", {
+      policyName: `${trackerThing.thingName}_Policy`,
+      policyDocument: {
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Effect: "Allow",
+            Action: "iot:*",
+            Resource: "*"
+          }
+        ]
+      }
+    });
+
+    const trackerLambdaRole = new iam.Role(
+      this,
+      "PetTrackerPositionLambdaRole",
+      {
+        assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com")
+      }
+    );
+
+    const trackerLambda = new lambda.Function(this, "PetTrackerPositionLambda", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, "position-lambda")),
+      handler: "index.handler",
+      memorySize: 128,
+      role: trackerLambdaRole,
+      timeout: cdk.Duration.seconds(15)
+    });
+
+    trackerLambda.addEnvironment("API_GRAPHQLAPIENDPOINT", "https://path-to-graphql.com");
+
+    const trackerLambdaAlias = new lambda.Alias(
+      this,
+      "PetTrackerPositionLambdaAlias",
+      {
+        aliasName: "PetTrackerPosition",
+        version: trackerLambda.currentVersion
+      }
+    );
+
+    trackerLambdaRole.addToPolicy(new iam.PolicyStatement({
+      resources: [`arn:aws:logs:${props?.env?.region}:${props?.env?.account}:*`],
+      actions: [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+    }));
+
+    trackerLambdaRole.addToPolicy(new iam.PolicyStatement({
+      resources: ['*'],
+      actions: [
+        "appsync:Create*",
+        "appsync:GraphQL",
+        "appsync:Get*",
+        "appsync:List*",
+        "appsync:Update*",
+        "appsync:Delete*"
+      ]
+    }));
+
+    const trackerTopicRule = new iot.CfnTopicRule(
+      this,
+      "PetTrackerTopicRule",
+      {
+        topicRulePayload: {
+          actions: [ 
+            {
+              lambda: {
+                functionArn: trackerLambdaAlias.functionArn
+              }
+            }
+          ],
+          ruleDisabled: false,
+          sql: "SELECT lat, long FROM 'device/+/pos'"
+        },
+        ruleName: "PetTrackerNotifyPosition",
+        
+      }
+    );
+
+    trackerLambda.addPermission("PetTrackerPositionLambdaPermission", {
+      principal: new iam.ServicePrincipal("iot.amazonaws.com"),
+      sourceAccount: props?.env?.account,
+      sourceArn: `arn:aws:iot:${props?.env?.region}:${props?.env?.account}:rule/${trackerTopicRule.ruleName}`
+    });
+  }
+}
