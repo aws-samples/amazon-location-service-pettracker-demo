91405a73ad886c653e7665df271564671266ec7c
Fabio Braga de Oliveira <fabiool@amazon.com>
2021-05-05 08:55:21 +0200

    Refactor code to create lambda to update position to its own class

--- cdk/lib/data-ingestion-stack.ts
+++ cdk/lib/data-ingestion-stack.ts
@@ -1,14 +1,16 @@
 import * as cdk from '@aws-cdk/core';
 import * as iot from '@aws-cdk/aws-iot';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as iam from '@aws-cdk/aws-iam';
 
-import { CustomCertificateResource } from "./custom-certificate-resource";
+import { CustomCertificateResource } from './custom-certificate-resource';
 import path = require("path");
+import { PetTrackerPositionLambda } from './pettracker-position-lambda'
 
 export class PetTrackerDataIngestionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const region = props?.env?.region || 'us-east-1'
+    const account = props?.env?.region || ''
 
     /**
      * Create Device.
@@ -51,79 +53,10 @@ export class PetTrackerDataIngestionStack extends cdk.Stack {
       }
     });
 
-    const trackerLambdaRole = new iam.Role(
-      this,
-      "PetTrackerPositionLambdaRole",
-      {
-        assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com")
-      }
-    );
-
-    const trackerLambda = new lambda.Function(this, "PetTrackerPositionLambda", {
-      runtime: lambda.Runtime.NODEJS_12_X,
-      code: lambda.Code.fromAsset(path.join(__dirname, "position-lambda")),
-      handler: "index.handler",
-      memorySize: 128,
-      role: trackerLambdaRole,
-      timeout: cdk.Duration.seconds(15)
+    new PetTrackerPositionLambda(this, 'pettracker-position-lambda', {
+      region: region,
+      account: account
     });
-
-    trackerLambda.addEnvironment("API_GRAPHQLAPIENDPOINT", "https://path-to-graphql.com");
-
-    const trackerLambdaAlias = new lambda.Alias(
-      this,
-      "PetTrackerPositionLambdaAlias",
-      {
-        aliasName: "PetTrackerPosition",
-        version: trackerLambda.currentVersion
-      }
-    );
-
-    trackerLambdaRole.addToPolicy(new iam.PolicyStatement({
-      resources: [`arn:aws:logs:${props?.env?.region}:${props?.env?.account}:*`],
-      actions: [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ]
-    }));
-
-    trackerLambdaRole.addToPolicy(new iam.PolicyStatement({
-      resources: ['*'],
-      actions: [
-        "appsync:Create*",
-        "appsync:GraphQL",
-        "appsync:Get*",
-        "appsync:List*",
-        "appsync:Update*",
-        "appsync:Delete*"
-      ]
-    }));
-
-    const trackerTopicRule = new iot.CfnTopicRule(
-      this,
-      "PetTrackerTopicRule",
-      {
-        topicRulePayload: {
-          actions: [ 
-            {
-              lambda: {
-                functionArn: trackerLambdaAlias.functionArn
-              }
-            }
-          ],
-          ruleDisabled: false,
-          sql: "SELECT lat, long FROM 'device/+/pos'"
-        },
-        ruleName: "PetTrackerNotifyPosition",
-        
-      }
-    );
-
-    trackerLambda.addPermission("PetTrackerPositionLambdaPermission", {
-      principal: new iam.ServicePrincipal("iot.amazonaws.com"),
-      sourceAccount: props?.env?.account,
-      sourceArn: `arn:aws:iot:${props?.env?.region}:${props?.env?.account}:rule/${trackerTopicRule.ruleName}`
-    });
+    
   }
 }
