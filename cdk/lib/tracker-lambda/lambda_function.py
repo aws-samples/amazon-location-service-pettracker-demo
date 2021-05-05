69b07c0b91eb4620ae812dfa18b101a4599c1869
Fabio Braga de Oliveira <fabiool@amazon.com>
2021-05-05 17:33:35 +0200

    Add ALS tracker lambda

---
+++ cdk/lib/tracker-lambda/lambda_function.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import json
+import os
+
+import boto3
+
+# Update this to match the name of your Tracker resource
+TRACKER_NAME = os.environ['TRACKER_NAME']
+
+
+def lambda_handler(event, context):
+  # load the side-loaded Amazon Location Service model; needed during Public Preview
+  os.environ["AWS_DATA_PATH"] = os.environ["LAMBDA_TASK_ROOT"]
+
+  updates = [
+    {
+      "DeviceId": event["deviceid"],
+      "SampleTime": datetime.fromtimestamp(event["timestamp"]).isoformat(),
+      "Position": [
+        event["location"]["long"],
+        event["location"]["lat"]
+      ]
+    }
+  ]
+
+  client = boto3.client("location")
+  response = client.batch_update_device_position(TrackerName=TRACKER_NAME, Updates=updates)
+
+  return {
+    "statusCode": 200,
+    "body": json.dumps(response)
+  }
