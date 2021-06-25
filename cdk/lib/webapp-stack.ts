0853949b52eeb4fa5dec144296cfb9aed0398af6
Fabio Braga de Oliveira <fabiool@amazon.com>
2021-06-25 17:04:03 +0200

    Change pipeline to use native Amplify deployment

--- cdk/lib/webapp-stack.ts
+++ cdk/lib/webapp-stack.ts
@@ -1,8 +1,7 @@
 import * as cdk from '@aws-cdk/core';
-import * as codebuild from '@aws-cdk/aws-codebuild';
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
-import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import * as codecommit from '@aws-cdk/aws-codecommit';
+import * as amplify from "@aws-cdk/aws-amplify";
 
 import path = require("path");
 
@@ -13,104 +12,17 @@ export class PetTrackerWebAppStack extends cdk.Stack {
     const region = props?.env?.region || 'us-east-1'
     const account = props?.env?.account || ''
 
-    const amplifyInit = new codebuild.PipelineProject(this, 'AmplifyInit', {
-      buildSpec: codebuild.BuildSpec.fromObject({
-        version: '0.2',
-        phases: {
-          install: {
-            commands: [
-              'cd web',
-              'npm install -g @aws-amplify/cli',
-              'npm install',
-            ],
-          },
-          build: {
-            commands: [
-              'cd web',
-              'amplify init --amplify $AMPLIFY --frontend $FRONTEND --providers $PROVIDERS --yes',
-              'amplify publish --yes'
-            ]
-          },
-        },
-        artifacts: {
-          'base-directory': 'web',
-          files: [
-            'amplify/**/*',
-          ],
-        },
-      }),
-      environment: {
-        buildImage: codebuild.LinuxBuildImage.STANDARD_2_0,
-        environmentVariables: {
-          AMPLIFY: {
-            type: codebuild.BuildEnvironmentVariableType.PLAINTEXT, 
-            value: '{\
-              \"projectName\":\"pettracker\",\
-              \"envName\":\"dev\",\
-              \"defaultEditor\":\"code\"\
-              }'
-          },
-          FRONTEND: {
-            type: codebuild.BuildEnvironmentVariableType.PLAINTEXT, 
-            value: '{\
-              \"frontend\":\"javascript\",\
-              \"framework\":\"react\",\
-              \"config\":{\
-                \"SourceDir\":\"src\",\
-                \"DistributionDir\":\"build\",\
-                \"BuildCommand\":\"npm run-script build\",\
-                \"StartCommand\":\"npm run-script start\"\
-                }\
-              }'
-          },
-          PROVIDERS: {
-            type: codebuild.BuildEnvironmentVariableType.PLAINTEXT, 
-            value: '{\
-              \"awscloudformation\":{\
-                \"configLevel\":\"project\",\
-                \"useProfile\":true,\
-                \"profileName\":\"default\"\
-                }\
-              }'
-          }
-        }
-      },
-    });
-
     const repo = codecommit.Repository.fromRepositoryName(
       this, 
       'ImportedRepo',
       'iot-workshop-for-pet-tracking-and-geofencing');
 
-    const sourceOutput = new codepipeline.Artifact();
-    const amplifyOutput = new codepipeline.Artifact('AmplifyOutput');    
-
-    new codepipeline.Pipeline(this, 'Pipeline', {
-      stages: [
-        {
-          stageName: 'Source',
-          actions: [
-            new codepipeline_actions.CodeCommitSourceAction({
-              actionName: 'Amplify_Source',
-              branch: 'develop',
-              repository: repo,
-              output: sourceOutput,
-            }),
-          ],
-        },
-        {
-          stageName: 'Deploy',
-          actions: [
-            new codepipeline_actions.CodeBuildAction({
-              actionName: 'Amplify_Deploy',
-              project: amplifyInit,
-              input: sourceOutput,
-              outputs: [amplifyOutput],
-            })
-          ],
-        }
-      ],
+    const amplifyApp = new amplify.App(this, "pettracker-app ", {
+      sourceCodeProvider: new amplify.CodeCommitSourceCodeProvider({
+        repository: repo,
+      }),
     });
+    const masterBranch = amplifyApp.addBranch("develop");
 
   }
 }
