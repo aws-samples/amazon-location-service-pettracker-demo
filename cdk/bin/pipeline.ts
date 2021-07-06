51cb61049c99aac195033306b023205b27ae4f71
Fabio Braga de Oliveira <fabiool@amazon.com>
2021-07-06 10:42:20 +0200

    Workaround to fix permission errors as detailed by https://github.com/aws/aws-cdk/tree/master/packages/%40aws-cdk/pipelines\#action-execution-denied

--- cdk/bin/pipeline.ts
+++ cdk/bin/pipeline.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { PetTrackerPipelineStack } from '../lib/pipeline-stack';
 import { PetTrackerDataIngestionStack } from '../lib/data-ingestion-stack';
+import { DefaultStackSynthesizer } from '@aws-cdk/core';
 
 const app = new cdk.App();
 
@@ -10,14 +11,20 @@ new PetTrackerPipelineStack(app, 'PetTrackerPipelineStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
-  }
+  },
+  synthesizer: new DefaultStackSynthesizer({
+    qualifier: 'cdkfix',
+  })
 });
 
 new PetTrackerDataIngestionStack(app, 'PetTrackerStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
-  }
+  },
+  synthesizer: new DefaultStackSynthesizer({
+    qualifier: 'cdkfix',
+  })
 });
 
 app.synth();
