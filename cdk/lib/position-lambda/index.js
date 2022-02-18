5a0d787a8c298bab34d9353295bf55e2527d86d2
Fabio Braga de Oliveira <fabiool@amazon.com>
2022-02-18 11:34:29 +0100

    Reident position lambda file with 2 spaces

--- cdk/lib/position-lambda/index.js
+++ cdk/lib/position-lambda/index.js
@@ -15,7 +15,7 @@ const updateGQL = gql(require('./graphql/mutations').updateLocation);
 const region = process.env.REGION
 
 AWS.config.update({
-    region
+  region
 });
 const SSM = new AWS.SSM();
 
@@ -35,106 +35,106 @@ const credentials = AWS.config.credentials;
  * @returns
  */
 exports.handler = (event) => {
-    console.log('event received:' + JSON.stringify(event));
+  console.log('event received:' + JSON.stringify(event));
 
-    function retrievePetTrackeGraphQLEndpoint() {
-        return parameterPromise.then(function (data, err) {
-            if (err) {
-                console.error(err)
-                throw err
-            } else return data.Parameter.Value;
-        });
-    }
+  function retrievePetTrackeGraphQLEndpoint() {
+    return parameterPromise.then(function (data, err) {
+      if (err) {
+        console.error(err)
+        throw err
+      } else return data.Parameter.Value;
+    });
+  }
 
-    function createAppSyncClient() {
-        return function (endpoint) {
-            console.log('SSM Parameter value:' + endpoint);
+  function createAppSyncClient() {
+    return function (endpoint) {
+      console.log('SSM Parameter value:' + endpoint);
 
-            return new AWSAppSyncClient({
-                    url: endpoint,
-                    region: region,
-                    auth: {
-                        type: 'AWS_IAM',
-                        credentials: credentials,
-                    },
-                    disableOffline: true,
-                },
-                {
-                    defaultOptions: {
-                        query: {
-                            fetchPolicy: 'network-only',
-                            errorPolicy: 'all',
-                        },
-                    },
-                }
-            ).hydrated();
-        };
-    }
+      return new AWSAppSyncClient({
+          url: endpoint,
+          region: region,
+          auth: {
+            type: 'AWS_IAM',
+            credentials: credentials,
+          },
+          disableOffline: true,
+        },
+        {
+          defaultOptions: {
+            query: {
+              fetchPolicy: 'network-only',
+              errorPolicy: 'all',
+            },
+          },
+        }
+      ).hydrated();
+    };
+  }
 
-    function updateExistingDevice(appSyncClient) {
-        console.log('Updating existing device, id: ', event.deviceId);
-        return appSyncClient.mutate({
-            mutation: updateGQL,
-            variables: {
-                input: {
-                    id: event.deviceId,
-                    lat: event.location.lat,
-                    long: event.location.long
-                }
-            }
-        });
-    }
+  function updateExistingDevice(appSyncClient) {
+    console.log('Updating existing device, id: ', event.deviceId);
+    return appSyncClient.mutate({
+      mutation: updateGQL,
+      variables: {
+        input: {
+          id: event.deviceId,
+          lat: event.location.lat,
+          long: event.location.long
+        }
+      }
+    });
+  }
 
-    function createNewDevice(appSyncClient) {
-        console.log('Creating new device, id:', event.deviceId);
-        return appSyncClient.mutate({
-            mutation: createGQL,
-            variables: {
-                input: {
-                    id: event.deviceId,
-                    lat: event.location.lat,
-                    long: event.location.long
-                }
-            }
-        });
-    }
+  function createNewDevice(appSyncClient) {
+    console.log('Creating new device, id:', event.deviceId);
+    return appSyncClient.mutate({
+      mutation: createGQL,
+      variables: {
+        input: {
+          id: event.deviceId,
+          lat: event.location.lat,
+          long: event.location.long
+        }
+      }
+    });
+  }
 
-    function searchForExistingDevice(appSyncClient) {
-        console.log('Searching for an existing device, id:', event.deviceId);
-        return appSyncClient.query({
-            query: queryGQL,
-            variables: {
-                id: event.deviceId
-            }
-        });
-    }
+  function searchForExistingDevice(appSyncClient) {
+    console.log('Searching for an existing device, id:', event.deviceId);
+    return appSyncClient.query({
+      query: queryGQL,
+      variables: {
+        id: event.deviceId
+      }
+    });
+  }
 
-    function createOrUpdateDevicePosition() {
-        return function (appSyncClient) {
-            return searchForExistingDevice(appSyncClient).then(function (queryResult) {
-                console.log('Query result:' + JSON.stringify(queryResult.data));
-                if (queryResult.data.getLocation) {
-                    return updateExistingDevice(appSyncClient);
-                } else {
-                    return createNewDevice(appSyncClient);
-                }
-            })
+  function createOrUpdateDevicePosition() {
+    return function (appSyncClient) {
+      return searchForExistingDevice(appSyncClient).then(function (queryResult) {
+        console.log('Query result:' + JSON.stringify(queryResult.data));
+        if (queryResult.data.getLocation) {
+          return updateExistingDevice(appSyncClient);
+        } else {
+          return createNewDevice(appSyncClient);
+        }
+      })
 
-        };
-    }
+    };
+  }
 
-    return retrievePetTrackeGraphQLEndpoint()
-        .then(createAppSyncClient())
-        .then(createOrUpdateDevicePosition())
-        .then(() => {
-            return {
-                statusCode: 200
-            }
-        })
-        .catch(error => {
-            console.error(error)
-            return {
-                statusCode: error.statusCode ? error.statusCode : 500
-            }
-        });
+  return retrievePetTrackeGraphQLEndpoint()
+    .then(createAppSyncClient())
+    .then(createOrUpdateDevicePosition())
+    .then(() => {
+      return {
+        statusCode: 200
+      }
+    })
+    .catch(error => {
+      console.error(error)
+      return {
+        statusCode: error.statusCode ? error.statusCode : 500
+      }
+    });
 }
