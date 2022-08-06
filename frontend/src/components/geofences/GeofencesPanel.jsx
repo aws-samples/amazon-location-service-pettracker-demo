562501fe180d39c5e4fe1925c0dbad6f19835482
Andrea Amorosi <dreamorosi@gmail.com>
2022-08-06 17:27:41 +0000

    feat: added  geofences & basic subscription

---
+++ frontend/src/components/geofences/GeofencesPanel.jsx
@@ -0,0 +1,121 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+import { useState } from "react";
+import {
+  Heading,
+  Button,
+  Loader,
+  Table,
+  TableCell,
+  TableBody,
+  TableRow,
+  CheckboxField,
+} from "@aws-amplify/ui-react";
+import Panel from "../common/Panel";
+import styles from "./GeofencesPanel.module.css";
+
+// Popup panel for Geofences
+const GeofencesPanel = ({
+  onClose,
+  geofences,
+  onDeleteGeofences,
+  onAddGeofence,
+  isLoading,
+  onToggleGeofences,
+  geofencesVisible,
+  totalGeofences,
+}) => {
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  // Help keep track of which checkboxes are selected
+  const handleCheckboxChange = (e) => {
+    if (selectedItems.includes(e.target.value)) {
+      setSelectedItems(selectedItems.filter((item) => item !== e.target.value));
+    } else {
+      setSelectedItems((prevSelectedItems) => [
+        ...prevSelectedItems,
+        e.target.value,
+      ]);
+    }
+  };
+
+  const handleDeleteGeofences = () => {
+    onDeleteGeofences(selectedItems);
+    setSelectedItems([]);
+  };
+
+  return (
+    <Panel
+      header={
+        <div className={styles.header}>
+          <Heading level={4}>Geofences</Heading>
+          <div>
+            <Button
+              size="small"
+              style={{ marginRight: "0.5rem" }}
+              onClick={handleDeleteGeofences}
+            >
+              Remove
+            </Button>
+            <Button size="small" variation="primary" onClick={onAddGeofence}>
+              Add
+            </Button>
+          </div>
+        </div>
+      }
+      footer={
+        <>
+          <Button size="small" onClick={onClose}>
+            Close
+          </Button>
+          <Button size="small" onClick={onToggleGeofences}>
+            {geofencesVisible ? "Hide" : "Show"} All Geofences
+          </Button>
+        </>
+      }
+    >
+      {isLoading ? (
+        <Loader size="large" />
+      ) : geofences?.length > 0 ? (
+        <>
+          <Table highlightOnHover={false} size="small" variation="striped">
+            <TableBody>
+              {geofences?.map((geofence) => {
+                return (
+                  <TableRow key={geofence.geofenceId}>
+                    <TableCell style={{ width: "1rem" }}>
+                      <CheckboxField
+                        value={geofence.geofenceId}
+                        checked={selectedItems.includes(geofence.geofenceId)}
+                        onChange={handleCheckboxChange}
+                      />
+                    </TableCell>
+                    <TableCell>{geofence.geofenceId}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+          {totalGeofences > 10 && (
+            <em className={styles.max}>
+              Displaying {geofences?.length} of {totalGeofences} geofences
+            </em>
+          )}
+        </>
+      ) : (
+        geofences?.length === 0 && (
+          <div className={styles.empty}>
+            <div className={styles.empty__header}>There are no geofences.</div>
+            <div className={styles.empty__body}>
+              Use the Add button to create a geofence. After you draw one or
+              more geofences, they will show up here.
+            </div>
+          </div>
+        )
+      )}
+    </Panel>
+  );
+};
+
+export default GeofencesPanel;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,22 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Amplify, PubSub } from "aws-amplify";
+// import PubSub from "@aws-amplify/pubsub";
+import { AmplifyProvider } from "@aws-amplify/ui-react";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
+PubSub.configure(awsconfig);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AmplifyProvider>
+      <App />
+    </AmplifyProvider>
+  </React.StrictMode>
+);
