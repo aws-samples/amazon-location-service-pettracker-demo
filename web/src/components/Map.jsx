a152472be775a194574b8ea54dc4d1a610f56664
Andrea Amorosi <dreamorosi@gmail.com>
2022-08-16 21:50:36 +0200

    removed unused imports

--- web/src/components/Map.jsx
+++ web/src/components/Map.jsx
@@ -1,8 +1,4 @@
 import React from "react";
-// import { MapView } from "@aws-amplify/ui-react";
-// import { NavigationControl, GeolocateControl } from "react-map-gl";
-// import { GeofenceControl } from "./GeofenceControl";
-// import { DeviceTracker } from "./DeviceTracker";
 
 const Map = ({ devPosMarkers, mapCenter }) => {
   return <></>;
