7394f4bfa418aeadc9362f82d0690aedbe20fc06
Andrea Amorosi <dreamorosi@gmail.com>
2022-09-25 12:21:31 +0200

    Update setCliRegion.js

--- setCliRegion.js
+++ setCliRegion.js
@@ -1,7 +1,7 @@
 // Script to set default region for AWS CLI based on instance placement
 // Steps:
 // 1. Retrieve token from IMDSv2
-// 2. Use token to retrieve AZ (i.e. `eu-west-1a`) from IMDSv2 using token, return region (i.e. `eu-west-1`)
+// 2. Use token to retrieve AWS Region (i.e. `eu-west-1`) from IMDSv2 using token
 // 3. Write file to `/home/ec2-user/.aws/config` setting the region for the `default` profile 
 
 const p = require("phin");
@@ -19,17 +19,16 @@ const getToken = async () => {
   return res.body.toString("utf8");
 };
 
-const getRegionFromAz = async (token) => {
+const getRegion = async (token) => {
   const res = await p({
-    url: "http://169.254.169.254/latest/metadata/placement/availability-zone",
+    url: "http://169.254.169.254/latest/meta-data/placement/region",
     method: "GET",
     headers: {
       "X-aws-ec2-metadata-token": token,
     },
   });
 
-  const az = res.body.toString("utf8");
-  return az.substring(0, az.length - 1);
+  return res.body.toString("utf8");
 };
 
 const writeAWSCliConfig = (region) => {
@@ -44,7 +43,7 @@ region=${region}`,
 
 const main = async () => {
   const token = await getToken();
-  const region = await getRegionFromAz(token);
+  const region = await getRegion(token);
   writeAWSCliConfig(region);
 };
 
