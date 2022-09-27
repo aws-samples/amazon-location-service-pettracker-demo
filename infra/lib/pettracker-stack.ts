de144e1bbde20b67832b51110826ce05be9fd361
Andrea Amorosi <dreamorosi@gmail.com>
2022-09-27 19:51:36 +0200

    feat: updated workspaces

---
+++ infra/lib/pettracker-stack.ts
@@ -0,0 +1,33 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { AuthConstruct } from "./auth-construct";
+import { StorageConstruct } from "./storage-construct";
+import { AppSyncConstruct } from "./api";
+import { FunctionsConstruct } from "./functions-construct";
+import { IotCoreConstruct } from "./iot-construct";
+
+export class PetTracker extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const { unauthRole } = new AuthConstruct(this, "authConstruct", {});
+
+    const { table } = new StorageConstruct(this, "storageConstruct", {});
+
+    const { api } = new AppSyncConstruct(this, "apiConstruct", {
+      table,
+    });
+
+    const { certificateHandlerFn, appSyncUpdateFn, trackerUpdateFn } =
+      new FunctionsConstruct(this, "functionsConstruct", {
+        graphqlUrl: api.graphqlUrl,
+      });
+    api.grantMutation(appSyncUpdateFn, "updatePosition");
+
+    new IotCoreConstruct(this, "iotCoreConstruct", {
+      certificateHandlerFn,
+      appSyncUpdateFn,
+      trackerUpdateFn,
+    });
+  }
+}
