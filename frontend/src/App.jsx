571797d4674b0250f9888f1c591f0edb45d6a527
Andrea Amorosi <dreamorosi@gmail.com>
2022-10-05 09:57:25 -0700

    workshop build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,31 +2,16 @@
 // SPDX-License-Identifier: MIT-0
 
 import React from "react";
-import { MapView } from "@aws-amplify/ui-react";
-import { NavigationControl } from "react-map-gl";
-import { GeofencesControl } from "./components/geofences/GeofencesControl";
-import { TrackerControl } from "./components/tracking/TrackerControl";
-import { DistanceControl } from "./components/routing/DistanceControl";
+// import { MapView } from "@aws-amplify/ui-react";
+// import { NavigationControl } from "react-map-gl";
+// import { GeofencesControl } from "./components/geofences/GeofencesControl";
+// import { TrackerControl } from "./components/tracking/TrackerControl";
+// import { DistanceControl } from "./components/routing/DistanceControl";
 
 const App = () => {
   return (
     <>
-      <MapView
-        initialViewState={{
-          longitude: -115.17077150978058,
-          latitude: 36.12309017212961,
-          zoom: 15,
-        }}
-        style={{
-          width: "100vw",
-          height: "100vh",
-        }}
-      >
-        <NavigationControl position={"top-left"} />
-        <GeofencesControl />
-        <TrackerControl />
-        <DistanceControl />
-      </MapView>
+	Hello World!
     </>
   );
 };
