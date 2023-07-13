da9a884a62953f2824047c05f49d6a256f552d7e
Andrea Amorosi <dreamorosi@gmail.com>
2023-07-13 19:52:43 +0200

    workshop version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,10 @@
 
 import React from "react";
 import { MapView } from "@aws-amplify/ui-react-geo";
-import { NavigationControl } from "react-map-gl";
-import { GeofencesControl } from "./components/geofences/GeofencesControl";
-import { TrackerControl } from "./components/tracking/TrackerControl";
-import { DistanceControl } from "./components/routing/DistanceControl";
+// import { NavigationControl } from "react-map-gl";
+// import { GeofencesControl } from "./components/geofences/GeofencesControl";
+// import { TrackerControl } from "./components/tracking/TrackerControl";
+// import { DistanceControl } from "./components/routing/DistanceControl";
 
 const coordinates = {
   longitude: -115.17077150978058,
@@ -14,23 +14,9 @@ const coordinates = {
 };
 
 const App = () => {
-  return (
-    <MapView
-      initialViewState={{
-        ...coordinates,
-        zoom: 15,
-      }}
-      style={{
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <NavigationControl position={"top-left"} />
-      <TrackerControl />
-      <GeofencesControl />
-      <DistanceControl />
-    </MapView>
-  );
+  return <>
+    Hello World!
+  </>;
 };
 
 export default App;
